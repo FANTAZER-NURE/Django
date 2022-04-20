d95f8463c2afc6a44c2cc2cdaa9a310ae79f654f
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-04-20 20:16:12 +0300

    +some shit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app1',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STATIC_ROOT = "D:/programming/FourthSem/djangoProject/app1/static/styles/styles.css"
+#
+#
+# STATICFILES_DIRS = [
+#     "D:/programming/FourthSem/djangoProject/app1/static/styles/styles.css"
+# ]
+
