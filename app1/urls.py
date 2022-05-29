136457d5923bd686dc3fa38be21d2eac96b74b5b
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-05-29 20:07:43 +0300

    +login

--- app1/urls.py
+++ app1/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
     path('', views.main, name='main'),
-    # path('main', views.main, name='main'),
     path('contacts/<str:color>/', views.contacts, name='contacts'),
     path('rooms', views.rooms, name='rooms'),
-
+    path('accounts/login/', LoginView.as_view(), name='login'),
 ]
